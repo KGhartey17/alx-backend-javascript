72015ea8ea99ed9642887cf229b85af131fb0dac
JeromeALX <kaphaelghartey@gmail.com>
2025-04-23 18:05:58 +0000

    Remove ESLint and Babel configuration files; refactor multiple JavaScript files for improved readability and consistency, including updates to function definitions and return values.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,24 +1,14 @@
-/**
- * Joins a set of strings with a dash after stripping the strings of
- * a leading sub string.
- * @param {Set<String>} set - A collection of strings.
- * @param {String} startString - The string to strip from the beginning
- * of each item in the set.
- * >
- * @returns {String}
- */
 export default function cleanSet(set, startString) {
-  const parts = [];
   if (
-    !set ||
-    !startString ||
-    !(set instanceof Set) ||
-    typeof startString !== "string"
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
   ) {
-    return "";
+    return '';
   }
+
+  const parts = [];
+
   for (const value of set.values()) {
-    if (typeof value === "string" && value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       const valueSubStr = value.substring(startString.length);
 
       if (valueSubStr && valueSubStr !== value) {
@@ -26,5 +16,5 @@ export default function cleanSet(set, startString) {
       }
     }
   }
-  return parts.join("-");
+  return parts.join('-');
 }
